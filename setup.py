e45b24033d4b519c8e1e4a6bc2daf8504969c39b
Wusung Peng <wusungpeng@kkbox.com>
2016-07-29 04:22:24 +0000

    feat: add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import sys
+
+from setuptools import setup
+
+PACKAGE = 'cwlogs'
+SOURCE = './'
+with open('VERSION') as f:
+    VERSION = f.read().rstrip()
+
+REQUIRES = [
+]
+
+setup(
+    name=PACKAGE,
+    version=VERSION,
+    py_modules=[PACKAGE],
+    entry_points={
+    'console_scripts': ['%s = %s:main' % (PACKAGE, PACKAGE)],},
+)
