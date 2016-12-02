08ecd47c376f07711412d36b325252958a414f30
Wusung Peng <wusungpeng@kkbox.com>
2016-12-02 13:41:51 +0800

    feat: query by start and end day

--- cwlogs.py
+++ cwlogs.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from datetime import datetime
 
@@ -18,21 +18,27 @@ parser.add_argument('-d', '--day', help='Specify day rage to limit the log')
 parser.add_argument('-l', '--log', help='Specify aws log group name')
 parser.add_argument('-p', '--prefix', help='Specify the default prefix of aws log group name. The default is ')
 parser.add_argument('-n', '--no-color', help='Specify day rage to limit the log')
-parser.add_argument('-f', '--file', help='Specify the output filename of the log')
+parser.add_argument('-s', '--start', help='Specify start day of the log')
+parser.add_argument('-e', '--end', help='Specify end day of the log')
 args = parser.parse_args()
 
 ## Set the necessary variables for aws log command
 SINCE="1d ago"
 LOGGROUPNAME="web-kkbox-contract.kkcontract"
 PREFIX = 'web-kkbox-contract'
-FILENAME = ''
 
-if args.hour:
-    SINCE = args.hour + 'h ago'
-elif args.day:
-    SINCE = args.day + "d ago"
+if args.start:
+    SINCE=args.start
 else:
-    SINCE = "1d ago"
+    if args.hour:
+        SINCE = args.hour + 'h ago'
+    elif args.day:
+        SINCE = args.day + "d ago"
+    else:
+        SINCE = "1d ago"
+
+if args.end:
+    END = args.end
 
 if args.log:
     LOGGROUPNAME = args.log
@@ -40,44 +46,27 @@ if args.log:
 if args.prefix:
     PREFIX = args.prefix
 
-if args.file:
-    FILENAME = args.file
-
-def main():
-    aws_args = []
+aws_args = []
+if args.end:
+    aws_args.append("awslogs get " + LOGGROUPNAME + " --start='%s' --end='%s' --no-color" % (SINCE, END))
+else:
     aws_args.append("awslogs get " + LOGGROUPNAME + " --start='%s' --no-color" % SINCE)
-    
-    output_file = None
-    if FILENAME:
-        output_file = open(FILENAME, 'w')
 
-    proc = subprocess.Popen(aws_args, stdout=subprocess.PIPE, shell=True)
-    (out, err) = proc.communicate()
-    for line in out.splitlines():
-        l = line.decode('utf-8')
-        group_name = l.split(' ')[0]
-        logger_name = l.split(' ')[1]
-        content = l.split(' ')[2:]
-        o = json.loads(' '.join(content))
-        d = dateutil.parser.parse(o.get('time'))
-        ts = calendar.timegm(d.utctimetuple()) 
-        if output_file:
-            output_file.write("%s %s [%s] %s - %s %s\n" % (
-              datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'),
-              o.get('level'),
-              o.get('thread'),
-              o.get('hostname'),
-              o.get('class'),
-              o.get('message')))
-        else:
-            print("%s %s [%s] %s - %s %s" % (
-              datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'),
-              o.get('level'),
-              o.get('thread'),
-              o.get('hostname'),
-              o.get('class'), 
-              o.get('message')))
-
-if __name__ == '__main__':
-    main()
+proc = subprocess.Popen(aws_args, stdout=subprocess.PIPE, shell=True)
+(out, err) = proc.communicate()
+for line in out.splitlines():
+    l = line.decode('utf-8')
+    group_name = l.split(' ')[0]
+    logger_name = l.split(' ')[1]
+    content = l.split(' ')[2:]
+    o = json.loads(' '.join(content))
+    d = dateutil.parser.parse(o.get('time'))
+    ts = calendar.timegm(d.utctimetuple()) 
+    print("%s %s [%s] %s - %s %s" % (
+          datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'),
+          o.get('level'),
+          o.get('thread'),
+          o.get('hostname'),
+          o.get('class'), 
+          o.get('message')))
 
